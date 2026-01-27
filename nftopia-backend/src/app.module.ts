9948f634c47659eb872bb938a7fa39758aa7918f
nuelo <akalo.emmanuel18@gmail.com>
2026-01-27 18:43:07 +0100

    fix: resolve conflict

--- nftopia-backend/src/app.module.ts
+++ nftopia-backend/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { typeOrmConfig } from './config/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    AuthModule,
+    ...(process.env.NODE_ENV === 'test'
+      ? []
+      : [TypeOrmModule.forRoot(typeOrmConfig), UsersModule]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
